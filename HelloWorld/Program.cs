5ac13fc66269d0192dc708ee73dc97977a566bbd
Patrick Campbell <csf23cap>
2024-09-24 15:02:57 +0100

    Initial commit

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.CodeAnalysis;
+
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("type first number");
+int firstnum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("type second number");
+int secondnum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("The sum of the numbers is " + (firstnum + secondnum));
+
+
+Console.WriteLine("Please enter your name");
+string name  = Console.ReadLine();
+
+Console.WriteLine("Please enter your age");
+int age= Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("hello" + name + " you will be ");
